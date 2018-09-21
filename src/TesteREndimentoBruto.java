b0938ec7e25a26cb7d0f16623527483e13e88229
Josue Nascimento <josuetk63@gmail.com>
2018-09-21 17:14:42 -0300

    Parametrizado com a barra verde

--- src/TesteREndimentoBruto.java
+++ src/TesteREndimentoBruto.java
@@ -1,23 +1,45 @@
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+import java.util.Arrays;
+import java.util.Collection;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
 public class TesteREndimentoBruto {
 
-	@Test
-	public void testeRendimentoBruto1() {
-		Calculadora calc = new Calculadora();
-
-		double result = calc.calculaRendimento(60,1000,8.5);
-		assertEquals(result,13.97,0.01);
+	private int dias;
+	private int capital;
+	private double taxa;
+	private double result;
+	public TesteREndimentoBruto(int dias, int capital, double taxa,double result) {
+		super();
+		this.dias = dias;
+		this.capital = capital;
+		this.taxa = taxa;
+		this.result = result;
 	}
-	@Test
-	public void testeRendimentoBruto2() {
-		Calculadora calc = new Calculadora();
-		
-		double result = calc.calculaRendimento(120,500,8.0);
-		assertEquals(result,13.15,0.01);
+
+	
+	
+	@Parameters
+	public static Collection<Object[]> data(){
+		return Arrays.asList(new Object [][] {
+			{60,1000,8.5,13.97},
+			{120,500,8.0,13.15},
+			{270,2000,8.5,125.75}
+		});
 	}
 	
+	@Test
+	public void testeRendimentoBruto3() {
+		Calculadora calc = new Calculadora();
+		double result = calc.calculaRendimento(this.dias, this.capital, this.taxa);
+		assertEquals(this.result, result,0.01);
+	
+	}
 }
