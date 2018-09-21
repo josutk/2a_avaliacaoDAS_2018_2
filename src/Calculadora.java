905562c32d39fdba27319f22933b503a353b496c
Josue Nascimento <josuetk63@gmail.com>
2018-09-21 17:05:17 -0300

    Barra vermelha previamente testada

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -7,6 +7,15 @@ public class Calculadora {
 
 	public double calculaRendimento(int i, int j, double d) {
 		return 13.97;
+		
+	}
+
+	private double convertDias(int i) {
+		double a =  (i / 365.0);
+		return a;
+	}
+	private double convertTaxa(double j) {
+		return (j /100);
 	}
 
 }
--- src/TesteREndimentoBruto.java
+++ src/TesteREndimentoBruto.java
@@ -6,10 +6,18 @@ import org.junit.Test;
 public class TesteREndimentoBruto {
 
 	@Test
-	public void testeRendimentoBruto() {
+	public void testeRendimentoBruto1() {
 		Calculadora calc = new Calculadora();
-		double result = calc.calculaRendimento(1000,60,8.5);
-		
-		assertEquals(result,13.97,0.0);
+
+		double result = calc.calculaRendimento(60,1000,8.5);
+		assertEquals(result,13.97,0.01);
 	}
+	@Test
+	public void testeRendimentoBruto2() {
+		Calculadora calc = new Calculadora();
+		
+		double result = calc.calculaRendimento(120,500,8.0);
+		assertEquals(result,13.15,0.01);
+	}
+	
 }
