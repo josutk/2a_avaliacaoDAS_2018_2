9760a429bbfc40678ba9739b39053cb6bf3a2fea
Josue Nascimento <josuetk63@gmail.com>
2018-09-21 17:40:36 -0300

    Falha vermelha

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -22,7 +22,21 @@ public class Calculadora {
 	}
 
 	public double calculaImposto(int i, int j, double d) {
-		return 3.14;
+		double result = calculaRendimento(i, j, d);
+		double a  = result*alicota(i);
+		System.out.println(a);
+		return a/100;
 	}
-
+	
+	private  double alicota(int d) {
+		if (d <=180 ) {
+ 			return 0.225;
+ 		}else if(d <= 360) {
+ 			return 0.2;
+ 		}else if(d <= 720) {
+ 			return 0.175;
+ 		}else {
+ 			return 0.15;
+ 		}
+	}
 }
--- src/TestRentabilidadeLiquida.java
+++ src/TestRentabilidadeLiquida.java
@@ -5,9 +5,15 @@ import org.junit.Test;
 public class TestRentabilidadeLiquida {
 		
 	@Test
-	public void testeRendimentoLiquido() {
+	public void testeImpostoDeRenda1() {
 		Calculadora c = new Calculadora();
-		double impostoDeRenda = c.calculaImposto(120,500, 22.5);
-		assertEquals(impostoDeRenda,3.14,0.001);
+		double impostoDeRenda = c.calculaImposto(120,500, 8.0);
+		assertEquals(impostoDeRenda,3.14,0.1);
+	}
+	@Test
+	public void testeImpostoDeRenda2() {
+		Calculadora c = new Calculadora();
+		double impostoDeRenda = c.calculaImposto(240,3000, 9.0);
+		assertEquals(impostoDeRenda,2.96,0.1);
 	}
 }
